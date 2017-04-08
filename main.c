70211d2522016b500aeb63db23d31552fb456814
Jason Pierna <siilver777@me.com>
2017-04-08 18:03:36 +0200

    Sélection d'un mot, création du string caché, choix de la lettre pendant la partie

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
+int randomNumber(int from, int to);
 
 int main(int argc, const char * argv[]) {
     printf("Début\n\n");
@@ -51,10 +54,58 @@ int main(int argc, const char * argv[]) {
     close(fd);
 
 
-    int i;
+   int i;
     for (i = 0; i <= mots; i++)
         printf("%s\n", words[i]);
 
 
+    // Sélection du mot aléatoirement
+    srand((unsigned int)time(NULL));
+    int randomChoice = randomNumber(0, mots+1);
+
+    printf("\nrandom: %d\n", randomChoice);
+    printf("%s\n", words[randomChoice]);
+
+    // Reconstitution de la chaîne de caractère cachée
+    temp = ' ';
+    char hidden[30];
+    i = 0;
+    do {
+        temp = words[randomChoice][i];
+        if (temp != '\0') {
+            hidden[i++] = '_';
+        }
+        else {
+            hidden[i++] = '\0';
+        }
+    } while (temp != '\0');
+
+    printf("%s\n", hidden);
+
+    int game = 1; // Game is on
+    int chances = 6;
+    char try;
+
+    do {
+        printf("%s\n", hidden);
+        printf("Entrez une lettre : ");
+        scanf("%c%*c", &try);
+
+
+        // test si la lettre est correcte ou non
+            // Si oui : on change la string hidden
+            // Si non : on diminue "chances"
+
+        // test de fin de jeu
+            // Est-ce que toutes les lettres sont trouvées ?
+            // Est-ce que chances est à 0 ?
+
+    } while (game == 1);
+
+
     return 0;
+}
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from) + from;
 }
