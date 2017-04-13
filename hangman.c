6faab3add1fce64b53729901431590be8141f080
Jason Pierna <siilver777@me.com>
2017-04-13 22:51:40 +0200

    Réorganisation et amélioration du code Le jeu fonctionne malgré quelques petites retouches à faire

---
+++ hangman.c
@@ -0,0 +1,143 @@
+//
+// Created by Jason Pierna on 13/04/2017.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "hangman.h"
+
+int openFile(const char * path) {
+    int fileDescriptor = open(path, O_RDONLY);
+
+    if (fileDescriptor == -1) {
+        printf("Error while opening file\n");
+        exit(-1);
+    }
+
+    return fileDescriptor;
+}
+
+char ** readFile(int fileDescriptor, int * wordsTotal) {
+    // Internal variables
+    char temp = 0;
+    int cursor = 0;
+    ssize_t bytesRead;
+
+    // Setting up pointers
+    char ** wordsList = malloc(10 * sizeof(char*));
+    *wordsTotal = 0;
+
+    // Reading inside file
+    do {
+        bytesRead = read(fileDescriptor, &temp, sizeof(char));
+
+        if (bytesRead == -1) {
+            // Error while reading file
+            printf("Error while reading file\n");
+            exit(-1);
+        } else if (bytesRead >= sizeof(char)) {
+            if (cursor == 0) {
+                // Allocate some memory to catch the word
+                wordsList[*wordsTotal] = (char *) malloc(sizeof(char) * 30);
+            }
+
+            if (temp == '\n') {
+                // Finish the current word and update the word count
+                wordsList[*wordsTotal][cursor] = '\0';
+
+                // Reallocate unused memory
+                wordsList[*wordsTotal] = (char*)realloc(wordsList[*wordsTotal], (cursor+1)* sizeof(char));
+
+                // Update the word count
+                (*wordsTotal)++;
+
+                // Reset the cursor
+                cursor = 0;
+            } else {
+                // Write the letter and update the cursor
+                wordsList[*wordsTotal][cursor++] = temp;
+            }
+        }
+    } while (bytesRead >= sizeof(char));
+
+    // Update the word count
+    if (*wordsTotal > 0)
+        (*wordsTotal)++;
+
+    return wordsList;
+}
+
+int randomNumber(int from, int to) {
+    return rand() % (to - from) + from;
+}
+
+char * getDashedWord(char * word) {
+    int i = 0;
+
+    // Get the word length
+    size_t wordLength = strlen(word);
+
+    // Allocate same memory to new dashed word
+    char * dashedWord = malloc(wordLength * sizeof(char));
+
+    // Dash the word
+    for (i = 0; i < strlen(word); i++) {
+        dashedWord[i] = '_';
+    }
+
+    return dashedWord;
+}
+
+void runGame(char * word) {
+    int i = 0, gameRunning = 1, availableTries = 10, totalReplacedLetters = 0;
+    size_t wordLength = strlen(word);
+    char letterTyped = 0;
+
+    char * dashedWord = getDashedWord(word);
+
+    do {
+        // Ask for a letter
+        printf("Type a letter: ");
+        scanf("%c%*c", &letterTyped);
+
+        int replacedLetters = 0;
+
+        // Check the answer by looping though the word
+        for (i = 0; i < wordLength; i++) {
+            if (word[i] == letterTyped) {
+                // The letter exists: does it need to be replaced?
+                if (dashedWord[i] == '_') {
+                    // Replace the letter
+                    dashedWord[i] = letterTyped;
+                    replacedLetters++;
+                }
+            }
+        }
+
+        // Check this try's results
+        if (replacedLetters == 0) {
+            // No letter has been replaced: wrong answer
+            availableTries--;
+            printf("Wrong guess!\n");
+        } else {
+            // Good guess, display the dashed word updated
+            printf("Good guess!\n%s\n", dashedWord);
+            totalReplacedLetters++;
+        }
+
+        // Check the game state
+        if (strcmp(dashedWord, word) == 0) {
+            gameRunning = 0;
+            printf("You win!");
+        }
+
+        if (availableTries == 0) {
+            gameRunning = 0;
+            printf("No more tries, you lose! The word was %s\n", word);
+        }
+
+    } while (gameRunning == 1);
+}
--- main.c
+++ main.c
@@ -3,109 +3,22 @@
 //
 
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-
-int randomNumber(int from, int to);
+#include "hangman.h"
 
 int main(int argc, const char * argv[]) {
-    printf("Début\n\n");
+    int wordsTotal = 0;
 
-    char * words[10];
-    char temp = ' ';
-    int position = 0;
-    int mots = 0;
+    int fileDescriptor = openFile("../words.txt");
+    char ** wordsList = readFile(fileDescriptor, &wordsTotal);
+    close(fileDescriptor);
 
-    // Ouverture du fichier
-    int fd = open("/Users/Silver/Desktop/file.txt", O_RDWR);
-    if (fd == -1) {
-        printf("Erreur lors de l'ouverture\n");
-        exit(-1);
-    }
+    srand((unsigned int) time(NULL));
+    char * selectedWord = wordsList[randomNumber(0, wordsTotal)];
 
-    ssize_t bytesRead;
-    do {
-        bytesRead = read(fd, &temp, sizeof(char));
-
-        if (bytesRead == -1) {
-            printf("Erreur lors de la lecture\n");
-            exit(-1);
-        }
-        else if (bytesRead == 0 || bytesRead < sizeof(char)) {
-            //printf("Erreur : fin du fichier atteinte\n");
-        }
-        else {
-            if (mots == 0 && position == 0) {
-                words[0] = (char *) malloc(sizeof(char) * 30);
-            }
-
-            if (temp == '\n') {
-                words[mots][position] = '\0';
-                words[++mots] = (char *) malloc(sizeof(char) * 30);
-                position = 0;
-            }
-            else {
-                words[mots][position++] = temp;
-            }
-        }
-    } while (bytesRead != 0 && bytesRead >= sizeof(char));
-    close(fd);
-
-
-   int i;
-    for (i = 0; i <= mots; i++)
-        printf("%s\n", words[i]);
-
-
-    // Sélection du mot aléatoirement
-    srand((unsigned int)time(NULL));
-    int randomChoice = randomNumber(0, mots+1);
-
-    printf("\nrandom: %d\n", randomChoice);
-    printf("%s\n", words[randomChoice]);
-
-    // Reconstitution de la chaîne de caractère cachée
-    temp = ' ';
-    char hidden[30];
-    i = 0;
-    do {
-        temp = words[randomChoice][i];
-        if (temp != '\0') {
-            hidden[i++] = '_';
-        }
-        else {
-            hidden[i++] = '\0';
-        }
-    } while (temp != '\0');
-
-    printf("%s\n", hidden);
-
-    int game = 1; // Game is on
-    int chances = 6;
-    char try;
-
-    do {
-        printf("%s\n", hidden);
-        printf("Entrez une lettre : ");
-        scanf("%c%*c", &try);
-
-
-        // test si la lettre est correcte ou non
-            // Si oui : on change la string hidden
-            // Si non : on diminue "chances"
-
-        // test de fin de jeu
-            // Est-ce que toutes les lettres sont trouvées ?
-            // Est-ce que chances est à 0 ?
-
-    } while (game == 1);
-
+    runGame(selectedWord);
 
     return 0;
-}
-
-int randomNumber(int from, int to) {
-    return rand() % (to - from) + from;
 }
